12247208757248f9692e87846f41ce106f52f2da
jbarkle <jbarkle4@kent.edu>
2023-03-25 16:19:17 -0700

    add sum and average to profit/loss

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,15 +2,14 @@
     # counter; for loop on csv record and count how many records there are
     # start counter at zero, for loop +1
 # The net total amount of "Profit/Losses" over the entire period
-    # for loop, add a specific column to a total variable
+    # for loop, add a specific column (index 1) to a total variable
 # The changes in "Profit/Losses" over the entire period, and then the average of those changes
-    # calculate total, then average changes
+    # calculate total, then average changes; store months and profit loss changes in a list
+    # update previous month to current month for each iteration
 # The greatest increase in profits (date and amount) over the entire period
-    # find the greatest increase, then store; loop one by one to find the next GI, storing as
-    # a new GI value is found
+    # find the max value in the profit loss change list, retrieve with month value in list
 # The greatest decrease in profits (date and amount) over the entire period
-    # find the greatest decrease, then store; loop one by one to find the next GD, storing as
-    # a new GD value is found
+    # find the min value in the profit loss change list, retrieve with month value in list
 
 # Step 1: read the CSV file
 import os
@@ -19,7 +18,11 @@ import csv
 # Variables
 total_months = 0
 net_total = 0
+PL_change = 0
+previous_month_PL = 0
 current_month_PL = 0
+months = []
+PL_changes = []
 
 csvpath = os.path.join("/Users/jennabarkley/Desktop/Challenges/Python-Challenge/PyBank/Resources/budget_data.csv")
 
@@ -29,7 +32,7 @@ with open(csvpath, 'r') as csvfile:
     # print(csvreader)
     # Save the header, and read the header row first
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}") #if needed
+    # print(f"CSV Header: {csv_header}") if needed
 
 # Read each row of data after the header
     for row in csvreader:
@@ -37,11 +40,21 @@ with open(csvpath, 'r') as csvfile:
 
     # Step 2: The total number of months included in the dataset
         total_months+= 1
-        # print(f"Total Months:  {total_months}")
     
     # Step 3: The net total amount of "Profit/Losses" over the entire period
         current_month_PL = int(row[1])
         net_total += current_month_PL
-        # print(net_total)
-            
 
+    # Step 4: The changes in "Profit/Losses" over the entire period, and then the average of those changes
+        if (total_months == 1):
+            previous_month_PL = current_month_PL
+            continue
+        else:
+            PL_change = current_month_PL - previous_month_PL
+            months.append(row[0])
+            PL_changes.append(PL_change)
+            previous_month_PL = current_month_PL
+    
+    sum_PL = sum(PL_changes)
+    avg_PL_change = sum_PL / (total_months - 1)
+    
