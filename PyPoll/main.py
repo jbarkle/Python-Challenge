a0133ab11c510bca06ecfa05e2c5f6ab6d88c015
jbarkle <jbarkle4@kent.edu>
2023-03-26 06:07:27 -0700

    list of candidates and total votes for each

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,14 +1,8 @@
 # The total number of votes cast
-    # counter; for loop on csv record and count how many records there are
-    # start counter at zero, for loop +1
 # A complete list of candidates who received votes
-
 # The percentage of votes each candidate won
-
 # The total number of votes each candidate won
-
 # The winner of the election based on popular vote.
-
 # In addition, your final script should both print the analysis to the terminal 
 # and export a text file with the results.
 
@@ -18,6 +12,9 @@ import csv
 
 # Variables
 total_votes = 0
+current_candidate = 0
+candidates_list = []
+votes_list = []
 
 csvpath = os.path.join("/Users/jennabarkley/Desktop/BootCamp Stuff/Module 3 Challenge/PyPoll/Resources/election_data.csv")
 
@@ -26,11 +23,24 @@ with open(csvpath, 'r') as csvfile:
     
     # Save the head, and read the header row first
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    #print(f"CSV Header: {csv_header}") if needed
 
     # Read each row of data after the header
     for row in csvreader:
         
         # Step 2: The total number of votes cast
         total_votes += 1
-        print(total_votes)
+        
+        # Step 3: A complete list of candidates who received votes, keep track
+        current_candidate = (row[2])
+
+        # Step 5: The total number of votes each candidate won
+        # start a list for candidates, and a list for votes
+        if (current_candidate in candidates_list):
+            candidate_index = candidates_list.index(current_candidate)
+            votes_list[candidate_index] = votes_list[candidate_index] + 1
+        else: #track changes and append
+            candidates_list.append(current_candidate)
+            votes_list.append(1)
+
+    
